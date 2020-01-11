ef6b08788fdb38d6d5399ab03399e26024b712dd
Glitch (hello-express) <none>
2020-01-11 20:58:08 +0000

    📠💂 Checkpoint ./classes/Projectile.js:6306021/332

---
+++ classes/Projectile.js
@@ -0,0 +1,10 @@
+class Projectile {
+  constructor(player, direction, speed) {
+    this.game = player.game
+    this.owner = player
+    this.direction = direction
+    this.speed = speed
+    
+    
+  }
+}
