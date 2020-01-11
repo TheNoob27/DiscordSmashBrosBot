9214b948b5473c98939662e515d32529220fc8bd
Glitch (hello-express) <none>
2020-01-11 19:26:01 +0000

    🙍🌍 Checkpoint ./classes/Character.js:6306021/162 ./classes/Game.js:6306021/508

--- classes/Character.js
+++ classes/Character.js
@@ -1,11 +1,11 @@
 const shields = ["🔴", "🔵", "🟡", "🟢"]
-
+const startingpos = [-3, -1, 1, 3]
 class Character {
   constructor(game, character) {
     this.game = game
     this.name = character
     this.tag = "P"+game.players.length + 1
-    this.x = 0
+    this.x = startingpos[game.players.length] || 0
     this.y = 0
     this.helpless = false
     this.doublejumped = false
@@ -17,7 +17,7 @@ class Character {
   }
   
   get onStage() {
-    return Math.floor(this.y) == 0 && [-8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8].includes(Math.floor(this.x))
+    return Math.floor(this.y) == 0 && [-4, -3, -2, -1, 0, 1, 2, 3, 4].includes(Math.floor(this.x))
   }
   
   get inAir() {
--- classes/Game.js
+++ classes/Game.js
@@ -11,10 +11,10 @@ class Game {
     let stage = ":stage_0_0::stage_1_0::stage_2_0::stage_3_0::stage_4_0::stage_5_0::stage_6_0::stage_7_0:\n:stage_0_1::stage_1_1::stage_2_1::stage_3_1::stage_4_1::stage_5_1::stage_6_1::stage_7_1:\n:stage_0_2::stage_1_2::stage_2_2::stage_3_2::stage_4_2::stage_5_2::stage_6_2::stage_7_2:"
     let blank = "<:blank:665229950265065483>"
     let screen = ""
-    let x = -8
-    let y = 8
+    let x = -4
+    let y = 6
     
-    for (var i = 0; i < 130; i++) {
+    for (var i = 0; i < 65; i++) {
       let inplace = this.players.find(p => Math.round(p.x) == x && Math.round(p.y) == y)
       if (inplace) {
         screen += inplace.icon
@@ -22,39 +22,15 @@ class Game {
         screen += blank
       }
       
-      y = x == 8 ? y - 1 : y
-      x = x == 8 ? -8 : x + 1
+      y = x == 4 ? y - 1 : y
+      x = x == 4 ? -4 : x + 1
       
-      if (y == -1 && x == -8) {
+      if (y == -1 && x == -4) {
           screen += stage
           break;
         }
     }
     
     return screen
-    /*
-    let x = -10
-    let y = 10
-    
-    for (var i = 0; i < 300; i++) {
-      let inplace = this.players.find(p => p.x == x && p.y == y)
-      if (inplace) {
-        let icon = inplace.emoji
-        screen += icon
-      } else {
-        if (y == -1 && x == -5) {
-          screen += "--"
-        }
-        screen += blank
-      }
-      
-      if (x == 10 && y == -10) break;
-      
-      y = x == 10 ? y - 1 : y
-      x = x == 10 ? -10 : x + 1
-    }
-    
-    return screen
-    */
   }
 }
