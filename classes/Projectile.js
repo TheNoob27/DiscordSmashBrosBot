f2b812df5ded2ac303d4de297336a265fc02613a
Glitch (hello-express) <none>
2020-01-11 21:33:02 +0000

    🍩📙 Checkpoint ./classes/Game.js:6306021/19 ./classes/Projectile.js:6306021/230

--- classes/Projectile.js
+++ classes/Projectile.js
@@ -2,11 +2,19 @@ class Projectile {
   constructor(player, direction, speed) {
     this.game = player.game
     this.owner = player
-    this.direction = direction
+    this.direction = direction.toLowerCase()
     this.speed = speed
     this.x = player.x
+    this.y = player.y
     this.interval = setInterval(() => {
-      
+      if (["right", "left"].includes(this.direction)) {
+        
+      }
     })
+  }
+  
+  destroy() {
+    clearInterval(this.interval)
+    delete this
   }
 }
