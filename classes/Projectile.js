d66da3972eb03f5e5777d9c5f4e6f84e1908288b
Glitch (hello-express) <none>
2020-01-11 23:10:01 +0000

    📠😚 Checkpoint ./classes/Projectile.js:6306021/148

--- classes/Projectile.js
+++ classes/Projectile.js
@@ -8,8 +8,11 @@ class Projectile {
     this.y = player.y
     this.interval = setInterval(() => {
       if (["right", "left"].includes(this.direction)) {
-        
+        this.x += this.direction == "right" ? 1 : -1
+      } else {
+        this.y += this.direction == "up" ? 1 : -1
       }
+      
     })
   }
   
