e376c39b528f70e2294fe2384ecfb2c9708f6fa9
Glitch (hello-express) <none>
2020-01-11 17:38:58 +0000

    🌿☃️ Checkpoint ./classes/Character.js:6306021/351 ./classes/Characters/Mario.js:6306021/207

--- classes/Character.js
+++ classes/Character.js
@@ -1,13 +1,13 @@
 class Character {
-  constructor(client, character) {
-    this.name = character.toLowerCase().charAt(0).toUpperCase() + character.toLowerCase().slice(1)
+  constructor(game, character) {
+    this.game = game
+    this.name = character
     this.x = 0
     this.y = 0
     this.helpless = false
     this.doublejumped = false
-    this.icon = icons[this.name]
-    this.emoji = "<:"+this.name.toLowerCase()+":"+this.icon+">"
     this.hp = 0
+    this.shieldhp = 0
     
     return this
   }
@@ -15,8 +15,8 @@ class Character {
   get onStage() {
     return Math.floor(this.y) == 0 && [-8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8].includes(Math.floor(this.x))
   }
-}
-
-let icons = {
-  Mario: "665103155230539776"
+  
+  get inAir() {
+    return !this.onStage
+  }
 }
---
+++ classes/Characters/Mario.js
@@ -0,0 +1,8 @@
+const Character = require("../Character.js")
+
+class Mario extends Character {
+  constructor(game) {
+    super(game, "Mario")
+  }
+  
+}
